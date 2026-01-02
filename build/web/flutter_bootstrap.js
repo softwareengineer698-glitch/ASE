839310b5bf6bd5deb85fa771eb2e2fdad8a0656e
Muhammad Ali <aliawan1170@gmail.com>
2026-01-02 10:16:05 +0500

    Add build/web for Vercel deployment and update gitignore

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4213381048"
+    serviceWorkerVersion: "3967360935"
   }
 });
